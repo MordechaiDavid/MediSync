10cc7babd7ee7f4dac2c72c462cde484a452fbbf
MordechaiDavid <moti208227@gmail.com>
2025-03-02 02:04:10 +0200

    Add logs for UserService.create

--- src/main/java/com/medisync/service/UserService.java
+++ src/main/java/com/medisync/service/UserService.java
@@ -5,13 +5,17 @@ import com.medisync.entity.Patient;
 import com.medisync.entity.User;
 import com.medisync.enums.UserType;
 import com.medisync.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Service
 public class UserService {
+    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
 
     @Autowired
     private UserRepository userRepository;
@@ -20,12 +24,25 @@ public class UserService {
     @Autowired
     PatientService patientService;
 
+    @Transactional
     public User create(User user) {
-        User newUser = userRepository.save(user);
-        if (user.getRole().equals(UserType.DOCTOR)){
-            doctorService.create(new Doctor(newUser.getId()));
-        } else if (user.getRole().equals(UserType.PATIENT)) {
-            patientService.create(new Patient(newUser.getId()));
+        logger.info("Starting user creation process for ID number: {}", user.getIdNumber());
+        User newUser =  null;
+        try {
+            newUser = userRepository.save(user);
+            logger.info("User created successfully with ID: {}", newUser.getIdNumber());
+            if (user.getRole().equals(UserType.DOCTOR)){
+                logger.info("Creating Doctor entity for user ID: {}", newUser.getId());
+                doctorService.create(new Doctor(newUser.getId()));
+                logger.info("Doctor entity created successfully for user ID: {}", newUser.getId());
+            } else if (user.getRole().equals(UserType.PATIENT)) {
+                logger.info("Creating Patient entity for user ID: {}", newUser.getId());
+                patientService.create(new Patient(newUser.getId()));
+                logger.info("Patient entity created successfully for user ID: {}", newUser.getId());
+            }
+        }catch (Exception e){
+            logger.error("Error during user creation for ID number: {}. Details: {}", user.getIdNumber(), e.getMessage());
+            throw new RuntimeException("Failed to create user: " + e.getMessage(), e);
         }
         return newUser;
     }
